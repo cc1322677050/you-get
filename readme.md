5846db5e255f211d9ad5e4f4afd6abe07931cb4f
chengcheng <1322677050@qq.com>
2020-02-24 22:37:29 +0800

    Initial commit

---
+++ readme.md
@@ -0,0 +1,10 @@
+#You-Get PyQt5小优化
+###作者:chengcheng
+
+    demo文件夹放到是ui文件
+    image文件夹下存放的是图标
+    v1是先前的版本
+    v2是有控制台输出的版本
+    注意 v1打包后要将iamge放入与main.exe同级的目录
+    v2打包后保main.exe同级目录则需要放入image和v2包中的v2
+    
---
+++ v1/Main.py
@@ -0,0 +1,80 @@
+import webbrowser
+import you_get
+
+from v1.YouGetUI import Ui_MainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow,QMessageBox,QWidget
+from PyQt5 import QtGui,QtCore
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import *
+from you_get.extractors import *
+
+
+
+class Main(QMainWindow,Ui_MainWindow,QWidget):
+    def __init__(self):
+        super(Main, self).__init__()
+        self.setupUi(self)
+        self.setWindowIcon(QIcon('image/icon.jpg'))
+
+    def callbacklog(self, msg):
+        print(msg)
+        # if msg=="1":
+        #     QMessageBox.information(self,"提示","下载完成！")
+
+    #设置背景
+    def resizeEvent(self,QResizeEvent):
+        palette = QtGui.QPalette()
+        pix = QtGui.QPixmap("image/background.jpg")
+        pix = pix.scaled(self.width(), self.height())
+        palette.setBrush(QtGui.QPalette.Background, QtGui.QBrush(pix))
+        self.setPalette(palette)
+    #关于项目
+    def aboutProject(self):
+        webbrowser.open('https://github.com/soimort/you-get')
+    #关于作者
+    def aboutAuthor(self):
+         QMessageBox.information(None,"关于作者","作者:chengcheng\n"
+                                             "QQ:1322677050")
+    #退出
+    def aboutExec(self):
+        sys.exit(app.exec_())
+    #下载
+    def download(self):
+        url=self.edit_url.text()
+        path=self.edit_path.text()
+        if re.match(r'^https?:/{2}\w.+$', url):
+            if path != '':
+                try:
+                    self.thread = Runthread(path,url)  # 创建线程
+                    self.thread._signal.connect(self.callbacklog)  # 连接信号
+                    self.thread.start()  # 开始线程
+                except BaseException as e:
+                    print(e)
+                    QMessageBox.critical(None, "错误", "线程无法启动")
+            else:
+                QMessageBox.critical(None, "错误", "输出地址有误")
+        else:
+            QMessageBox.critical(None, "错误", "视频地址错误")
+
+
+
+# 继承QThread
+class Runthread(QtCore.QThread):
+    _signal = pyqtSignal(str)
+    def __init__(self,path,url):
+        super(Runthread, self).__init__()
+        self.path = path
+        self.url = url
+    def __del__(self):
+        self.wait()
+    def run(self):
+        sys.argv = ['you-get', '-o', self.path, self.url,"--debug"]
+        you_get.main()
+        self._signal.emit("1")
+
+
+if __name__ == '__main__' :
+    app = QApplication(sys.argv)
+    main = Main()
+    main.show()
+    sys.exit(app.exec())
